ecf421554d1459fa8bbf5a1435e16e24115ba904
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2023-01-05 21:49:17 +0000

    Added API fetch call to get quote

---
+++ index.js
@@ -0,0 +1,12 @@
+let quoteButton = document.getElementById("quote-button");
+
+quoteButton.addEventListener("click", () => {
+    let stoicQuoteURL = "https://stoicquotesapi.com/v1/api/quotes/random";
+    
+    fetch(stoicQuoteURL)
+        .then((response => response.json()))
+        .then((data) => {
+            console.log(data)
+            document.getElementById("quote-text").innerHTML = data.body
+        })
+})
